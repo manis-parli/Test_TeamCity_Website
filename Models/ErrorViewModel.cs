6b79b4cc1f0a02add2650024dbdd664e9084ec39
Sri Mani <manis@parliament.uk>
2025-04-30 14:47:41 +0100

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_TeamCity_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
